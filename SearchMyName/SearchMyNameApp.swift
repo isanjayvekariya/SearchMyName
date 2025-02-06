70b4ca4eeb8ba257a5b25b525a68cea7b1a24306
Sanjay Vekariya <sanjay_vekariya@hotmail.com>
2025-02-06 16:56:43 -0600

    Initial Commit

---
+++ SearchMyName/SearchMyNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMyNameApp.swift
+//  SearchMyName
+//
+//  Created by Sanjay Vekariya on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchMyNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
